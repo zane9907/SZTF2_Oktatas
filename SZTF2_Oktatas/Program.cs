e494bc1c8d291404e5d5f3e44b804bea73c2d05f
Botond Antalóczy <zane9907@gmail.com>
2023-09-16 13:30:03 +0200

    Add project files.

---
+++ SZTF2_Oktatas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SZTF2_Oktatas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
